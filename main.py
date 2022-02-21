5b74efb3bf6808772bc906dcf67fca7ccf6825c2
Akanksha Koshti <97248384+koshtiakanksha@users.noreply.github.com>
2022-02-21 10:49:01 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,10 +5,10 @@ import random
 now = dt.datetime.now()
 current_day = now.weekday()
 
-my_email = "koshtiakanksha212@gmail.com"
-my_password = "12M@y2001"
+my_email = "----------@gmail.com"
+my_password = "********"
 
-if current_day == 6:
+if current_day == 0:
     with open("quotes.txt", "r") as quote_file:
         all_quotes = quote_file.readlines()
         quote = random.choice(all_quotes)
@@ -19,6 +19,6 @@ if current_day == 6:
         connection.login(user=my_email, password=my_password)
         connection.sendmail(
             from_addr=my_email,
-            to_addrs="koshtiakanksha12@gmail.com",
+            to_addrs="______________",
             msg=f"Subject:Monday Motivation\n\n{quote}"
         )
